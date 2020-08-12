7b5759d85fe9cedf09a21c86c90afda1fb540bdb
Truc Chau <tc9538@cs.ship.edu>
2020-08-12 14:02:09 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter the amount of cents to be converted to coins");
+            int coins = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
